5fa6a26f4bdc73481a74bd86da26b967f92a8951
秀艳 王 <wangxiuyan@aixcoder.com>
2024-06-27 17:48:31 +0800

    indexError

--- agentUtils.py
+++ agentUtils.py
@@ -4,3 +4,8 @@ class AgentUtils:
     def indexError(self):
         tmp_list = [1,2,3]
         print(tmp_list[5])
+
+
+if __name__ == '__main__':
+    agentUtils = AgentUtils()
+    agentUtils.indexError()
