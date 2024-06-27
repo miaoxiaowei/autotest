133c9259ce3ce302a1dc68a799d3f64e39e43c98
Xiaowei Miao <miaoxiaowei@aixcoder.com>
2024-06-27 18:47:07 +0800

    remove if

--- string_util.py
+++ string_util.py
@@ -1,9 +1,8 @@
-"""
-找出str1中，str2出现的次数
-"""
+
 def find_str(str1, str2):
-    if not str1 or not str2:
-        return 0
+    """
+    找出str1中，str2出现的次数
+    """
     count = 0
     for i in range(len(str1)):
         if str1[i:i + len(str2)] == str2:
