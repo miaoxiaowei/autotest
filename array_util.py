b8544f7454ae5578fd9fa9be8dcc6673fe82d940
秀艳 王 <wangxiuyan@aixcoder.com>
2024-06-28 11:19:47 +0800

    xijiexiugai

--- array_util.py
+++ array_util.py
@@ -1,4 +1,4 @@
 def print_arr(arr):
   # 这里应该是 range(len(numbers))
-  for i in range(len(numbers) - 1):  
-    print(numbers[i])
+  for i in range(len(arr) - 1):
+    print(arr[i])
--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
 def str_to_int(str):
   # ValueError: when not digit
-  num = int(s)
+  num = int(str)
   print("转换成功:", num)
