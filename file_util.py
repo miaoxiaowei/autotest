194d1291362f73bef51a7bffd72bc1d443263492
Xiaowei Miao <miaoxiaowei@aixcoder.com>
2024-06-28 19:08:21 +0800

    add file util

--- file_util.py
+++ file_util.py
@@ -4,8 +4,13 @@ import os
 
 def read_csv(filename):
     # 检查文件是否存在
-    # 打开并读取CSV文件
-    with open(filename, 'r', newline='') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
+    if os.path.exists(filename):
+        print(f"文件 {filename} 存在。下面是文件内容：")
+
+        # 打开并读取CSV文件
+        with open(filename, 'r', newline='') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row)
+    else:
+        print(f"文件 {filename} 不存在。")
